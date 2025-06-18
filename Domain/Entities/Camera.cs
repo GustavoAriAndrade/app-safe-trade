2586130b475c872a498ec2ba3abd22b7e10c1f22
Gustavo Andrade <gustavo.andrade@finer.com.br>
2025-06-17 22:10:57 -0300

    [feat] Entidades e conexão com banco

---
+++ Domain/Entities/Camera.cs
@@ -0,0 +1,10 @@
+﻿namespace api_safe_trade.Domain.Entities
+{
+    public class Camera
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string IpAddress { get; set; }
+        public List<AvistamentoSuspeito>? Avistamentos { get; set; }
+    }
+}
---
+++ Domain/Entities/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using api_safe_trade.Domain.Enums;
+
+namespace api_safe_trade.Domain.Entities
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+        public bool Status { get; set; }
+        public RoleEnum Role { get; set; }
+        public List<AbordagemSuspeito>? Abordagens { get; set; }
+    }
+}
